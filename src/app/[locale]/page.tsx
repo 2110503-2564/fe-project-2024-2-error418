8ba8745f4f7eccdcaf8350dd760b8de25f4c04ba
Cookie4686 <cookieandcake1105@gmail.com>
2025-03-16 20:19:06 +0700

    fix bad params typing

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
-import { Context, use } from "react";
+import { use } from "react";
 import { Locale, useTranslations } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 
-export default function Home({ params }: { params: Context<{ locale: Locale }> }) {
+export default function Home({ params }: { params: Promise<{ locale: Locale }> }) {
   const { locale } = use(params);
   // Enable static rendering
   setRequestLocale(locale);
