a9139663132ead4b2746fba2f4b51201e90d558f
Cookie4686 <cookieandcake1105@gmail.com>
2025-03-18 11:39:22 +0700

    add UI for theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,21 +1,36 @@
 "use client";
 
 import { useTransition } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/16/solid";
+import styles from "./themeSwitcher.module.css";
 import { useThemeContext } from "@/provider/theme";
 
 export default function ThemeSwitcher() {
   const [isPending, startTransition] = useTransition();
-  const { toggleTheme } = useThemeContext();
+  const { theme, toggleTheme } = useThemeContext();
 
-  function handleOnClick() {
-    startTransition(async () => {
-      await toggleTheme();
-    });
+  function handleOnChange() {
+    if (!isPending) {
+      startTransition(async () => {
+        await toggleTheme();
+      });
+    }
   }
 
   return (
-    <button className="disabled:opacity-10" onClick={handleOnClick} disabled={isPending}>
-      Change
-    </button>
+    <label className={styles.label} htmlFor="theme-selector">
+      <input
+        className="h-0 w-0 overflow-hidden opacity-0"
+        role="button"
+        type="checkbox"
+        name="theme-selector"
+        id="theme-selector"
+        onChange={handleOnChange}
+        checked={theme == "theme-dark"}
+      />
+      <SunIcon className={styles.sun} />
+      <MoonIcon className={styles.moon} />
+      <span className={styles.ball}></span>
+    </label>
   );
 }
