bc3cfa98e1439b2484ca38b72650df8b01aab3d8
excursion <migth.np4@gmail.com>
2025-03-24 06:21:38 +0700

    fix my restaurant card's link and add style to restaurant edit, create and reserve

--- src/app/[locale]/dashboard/restaurants/page.tsx
+++ src/app/[locale]/dashboard/restaurants/page.tsx
@@ -1,8 +1,7 @@
 import { auth } from "@/auth";
 import { getRestaurants } from "@/db/restaurants";
 import { redirect } from "next/navigation";
-import RestaurantSearch from "@/components/RestaurantSearch";
-
+import MyRestaurantSearch from "@/components/MyRestaurantSearch";
 export default async function MyRestaurants() {
   const session = await auth();
   if (!session) {
@@ -16,7 +15,7 @@ export default async function MyRestaurants() {
   return (
     <main>
       <h1>My Restaurants</h1>
-      <RestaurantSearch restaurants={restaurants.data} />
+      <MyRestaurantSearch restaurants={restaurants.data} />
     </main>
   );
 }
