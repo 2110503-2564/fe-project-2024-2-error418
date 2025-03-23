8b7831cc2e25629823674954bf49b4ae1bf58319
excursion <migth.np4@gmail.com>
2025-03-23 22:44:12 +0700

    add search bar for restaurants and reservations

--- src/app/[locale]/reservations/page.tsx
+++ src/app/[locale]/reservations/page.tsx
@@ -1,10 +1,14 @@
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import { editReservation, getUserReservations, PopulatedReservationJSON } from "@/db/reservations";
-import { Link } from "@/i18n/navigation";
-import Image from "next/image";
-import { Button } from "@mui/material";
+import ReservationSearch from "@/components/ReservationSearch";
 import { revalidatePath } from "next/cache";
+
+async function cancelReservation(id: string) {
+  "use server";
+  await editReservation(id, "canceled");
+  revalidatePath("/reservations");
+}
 
 export default async function Reservations() {
   const session = await auth();
@@ -19,53 +23,10 @@ export default async function Reservations() {
   return (
     <main>
       <h1>My Reservations</h1>
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(16.5rem,1fr))] justify-items-center gap-8 py-4">
-        {reservations.data.map((e) => (
-          <li key={e.id}>
-            <Card {...e}></Card>
-          </li>
-        ))}
-      </ul>
+      <ReservationSearch
+        reservations={reservations.data}
+        cancelReservation={cancelReservation}
+      ></ReservationSearch>
     </main>
-  );
-}
-
-function Card({ restaurant, reserveDate, approvalStatus, id }: PopulatedReservationJSON) {
-  return (
-    <div className="h-[22rem] w-[16.5rem] rounded-lg bg-white p-1 shadow-lg hover:bg-neutral-200 hover:shadow-2xl">
-      <Link href={`/reservations/${id}`}>
-        <div className="h-[50%]">
-          <Image
-            className="pointer-events-none h-full w-full rounded-sm object-cover object-center select-none"
-            src="/img/restaurant.jpg"
-            alt={restaurant.name}
-            width={0}
-            height={0}
-            sizes="100vw"
-            priority
-          />
-        </div>
-      </Link>
-      <div className="flex h-[35%] flex-col overflow-auto">
-        <h2 className="pt-4 pb-2 text-[1rem] font-bold">{restaurant.name}</h2>
-        <span>{reserveDate.toLocaleString()}</span>
-        <span>{approvalStatus}</span>
-      </div>
-      <div className="z-10 flex h-[15%] flex-col overflow-auto">
-        {(approvalStatus == "pending" || approvalStatus == "approved") && (
-          <form
-            action={async () => {
-              "use server";
-              await editReservation(id, "canceled");
-              revalidatePath("/reservations");
-            }}
-          >
-            <Button variant="contained" type="submit">
-              Cancel
-            </Button>
-          </form>
-        )}
-      </div>
-    </div>
   );
 }
--- src/app/[locale]/restaurants/page.tsx
+++ src/app/[locale]/restaurants/page.tsx
@@ -1,48 +1,17 @@
-import { RestaurantJSON } from "@/db/models/Restaurant";
 import { getRestaurants } from "@/db/restaurants";
-import { Link } from "@/i18n/navigation";
-import Image from "next/image";
+import RestaurantSearch from "@/components/RestaurantSearch";
 
 export default async function Restaurants() {
   const restaurants = await getRestaurants();
+
   if (!restaurants.success) {
     return <main>Cannot fetch data</main>;
   }
+
   return (
     <main>
       <h1>Restaurants</h1>
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(16.5rem,1fr))] justify-items-center gap-8 py-4">
-        {restaurants.data.map((e) => (
-          <li key={e.id}>
-            <Link href={`/restaurants/${e.id}`}>
-              <Card {...e}></Card>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <RestaurantSearch restaurants={restaurants.data} />
     </main>
-  );
-}
-
-function Card({ name, address, region }: RestaurantJSON) {
-  return (
-    <div className="h-[22rem] w-[16.5rem] rounded-lg bg-white p-1 shadow-lg hover:bg-neutral-200 hover:shadow-2xl">
-      <div className="h-[67%]">
-        <Image
-          className="pointer-events-none h-full w-auto rounded-sm object-cover object-center select-none"
-          src="/img/restaurant.jpg"
-          alt={name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          priority
-        />
-      </div>
-      <div className="flex h-[33%] flex-col overflow-auto">
-        <h2 className="pt-4 pb-2 text-[1rem] font-bold">{name}</h2>
-        <span>{address}</span>
-        <span>{region}</span>
-      </div>
-    </div>
   );
 }
