d08804ee1463f126705b9a67858c11d54bb52b42
excursion <migth.np4@gmail.com>
2025-03-24 05:25:49 +0700

    add style to dashboards

--- src/app/[locale]/dashboard/restaurants/[id]/page.tsx
+++ src/app/[locale]/dashboard/restaurants/[id]/page.tsx
@@ -15,9 +15,9 @@ export default async function RestaurantDashboard({ params }: { params: Promise<
 
   return (
     <main>
-      <h1>Restaurant Dashboard</h1>
-      <section>
-        <h2 className="text-center text-xl font-bold">Past Approval Status</h2>
+      <section className="mb-8 rounded-lg p-6 shadow-xl outline-2">
+        <h1>Restaurant Dashboard</h1>
+        <h2 className="mb-4 text-center text-xl font-bold text-yellow-600">Past Approval Status</h2>
         {approvalStatus.success ?
           approvalStatus.data ?
             <div className="mx-auto flex w-fit flex-col gap-2 py-2">
@@ -31,14 +31,14 @@ export default async function RestaurantDashboard({ params }: { params: Promise<
         : <span>Cannot fetch data</span>}
       </section>
 
-      <section>
-        <h2 className="text-center text-xl font-bold">Reservation Frequency</h2>
+      <section className="rounded-lg bg-yellow-50 p-6 shadow-xl">
+        <h2 className="mb-4 text-center text-xl font-bold text-black">Reservation Frequency</h2>
         {frequency.success ?
           <BarChart
             height={600}
             dataset={frequency.data}
             xAxis={[{ scaleType: "band", dataKey: "k" }]}
-            series={[{ dataKey: "v" }]}
+            series={[{ dataKey: "v", label: "Reservations", color: "#FCD34D" }]}
           />
         : <span>No past Reservation for this restaurant</span>}
       </section>
